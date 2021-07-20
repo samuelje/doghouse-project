67b22e5dffaba445ea612a79bdbbef125197da2e
bethgietl <bfave19@gmail.com>
2021-07-19 22:33:05 -0600

    commit

--- JBZ_FOLDER/static/js/logic_advanced.js
+++ JBZ_FOLDER/static/js/logic_advanced.js
@@ -16,7 +16,7 @@ var layers = {
 
 // Create the map with our layers
 var map = L.map("map-id", {
-  center: [37.09, -95.71],
+  center: [37.0902, -95.7129],
   zoom: 5,
   layers: [
     layers.adoptable_dogs,
@@ -40,20 +40,22 @@ var overlays = {
 };
 
 // Create a control for our layers, add our overlay layers to it
-L.control.layers(null, overlays).addTo(map);
+L.control.layers(null, overlays, {
+  collapsed: false
+}).addTo(map);
 
 // Create a legend to display information about our map
-var info = L.control({
-  position: "bottomright"
-});
+//var info = L.control({
+  //position: "bottomright"
+//});
 
 // When the layer control is added, insert a div with the class of "legend"
-info.onAdd = function() {
-  var div = L.DomUtil.create("div", "legend");
-  return div;
-};
+//info.onAdd = function() {
+  //var div = L.DomUtil.create("div", "legend");
+  //return div;
+//};
 // Add the info legend to the map
-info.addTo(map);
+//info.addTo(map);
 
 // Initialize an object containing icons for each layer group
 var icons = {
@@ -82,9 +84,9 @@ var icons = {
     shape: "circle"
   }),
   XL_dogs: L.ExtraMarkers.icon({
-    icon: "ion-android-bicycle",
-    iconColor: "white",
-    markerColor: "green",
+    icon: "ion-ios-paw",
+    iconColor: "black",
+    markerColor: "blue",
     shape: "circle"
   })
 };
@@ -97,24 +99,24 @@ var icons = {
     var dogSizes;
 
     // Loop through the stations (they're the same size and have partially matching data)
-    for (var i = 0; i < dogLocation.length; i++) {
+    for (var i = 0; i < 1500; i++) {
 
       // Create a new station object with properties of both station objects
       var dog = Object.assign({}, dogLocation[i]);
       // If a station is listed but not installed, it's coming soon
-      if (dog.name) {
+      if (dog.properties.size) {
         dogSizes = "adoptable_dogs";
       }
       // If a station has no bikes available, it's empty
-      else if (dog.size === 1) {
+      else if (dog.properties.size === 1) {
         dogSizes = "Small_dogs";
       }
       // If a station is installed but isn't renting, it's out of order
-      else if (dog.size === 0) {
+      else if (dog.properties.size === 0) {
         dogSizes = "Medium_dogs";
       }
       // If a station has less than 5 bikes, it's status is low
-      else if (dog.size === 2) {
+      else if (dog.properties.size === 2) {
         dogSizes = "Large_dogs";
       }
       // Otherwise the station is normal
@@ -122,8 +124,10 @@ var icons = {
         dogSizes = "XL_dogs";
       }
 
+      //console.log(dogSizes);
+
       // Create a new marker with the appropriate icon and coordinates
-      var newMarker = L.geoJson(dog.geometry, {
+      var newMarker = L.geoJSON(dog.geometry, {
         icon: icons[dogSizes]
       });
 
@@ -131,6 +135,7 @@ var icons = {
       newMarker.addTo(layers[dogSizes]);
 
       // Bind a popup to the marker that will  display on click. This will be rendered as HTML
-      newMarker.bindPopup(dog.properties.name + "<br> Capacity: " + dog.properties.url + "<br>" + dog.properties.breeds_primary + " Bikes Available");
+      newMarker.bindPopup("<img src=" + dog.properties.primary_photo_cropped_full + "/>" + "<br>" + "size: " + dog.properties.size + "<br>" + "Name: " + dog.properties.name + "<br>" + "Breed: " + dog.properties.breeds_primary + "<br>" + "Age: " + dog.properties.age + "<br>" + "Website: " + "<a href=" + dog.properties.url + ">" + "website link");
     }
 
+    //dog.properties.primary_photo_cropped_full + 
